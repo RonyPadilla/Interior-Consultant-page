49607cc1a09662ea52a90ba40c07af3b19ec6c31
Rony Moises Padilla Cruz <110305534+RonyPadilla@users.noreply.github.com>
2023-07-16 20:31:46 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,10 @@ import Footer from "./components/footer";
 import Content from "./Components/Content";
 import "./Style/index.css";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  
   <>
-<Nav style={{ backgroundColor: 'white' }} />
-
+    <Nav />
     <Content />
-    
     <div className="credits">
       <Footer />
     </div>
