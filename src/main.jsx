bc074c3c2ddab672d1891c00b60308f1de887e2c
Rony Moises Padilla Cruz <110305534+RonyPadilla@users.noreply.github.com>
2023-07-17 22:00:09 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Nav from "./Components/Nav";
-import Footer from "./components/footer";
 import Content from "./Components/Content";
+import Footer from "./components/footer";
 import "./Style/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
